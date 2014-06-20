75bb8e559a23eb62ca3a8e3c6f94fc0d8b141907
beyondweike <brogrammer@qq.com>
2014-06-21 07:23:45 +0800

    a

--- controller/getConfigData.php
+++ controller/getConfigData.php
@@ -115,7 +115,7 @@
             //$retContent.=bytesContentWithFileType("../$dir/config/resource/","categories.json");
 			
 			//serverSetting.json
-            //$retContent.=bytesContentWithFileType("../$dir/config/resource/","serverSetting.json");
+            $retContent.=bytesContentWithFileType("../$dir/config/resource/","serverSetting.json");
             
             //loading.html
             $retContent.=bytesContentWithFileType("../$dir/config/resource/","loading.html");
